4178421b40f13483aca198fc9b6f7d2a91dfa4e5
souvickroy02 <souvickroy02@gmail.com>
2020-08-20 20:26:49 +0530

    mps-tourism
    it is an indian tourism project

---
+++ main.js
@@ -0,0 +1,19 @@
+var header = document.getElementById("lstbtn");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+const btnScrollToTop = document.querySelector("#buttonToTop");
+btnScrollToTop.addEventListener("click", function (){
+    // window.scrollTo(0, 0);
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
